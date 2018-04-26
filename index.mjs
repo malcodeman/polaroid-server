24bac6a707ee52531e430e13a3da1d06f5e45f97
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-04-27 00:08:51 +0200

    Adds cors.

--- index.mjs
+++ index.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import posts_routes from "./routes/posts_routes.mjs";
 
 const PORT = process.env.PORT || 9000;
 const app = express();
 
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true
