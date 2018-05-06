c6e3b1e94c34a829d10a7e7d38218c95216b612b
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-05-06 18:59:55 +0200

    Return jwt on signup

--- controllers/users_controller.mjs
+++ controllers/users_controller.mjs
@@ -1,7 +1,13 @@
 import User from "../models/users_model.mjs";
+import jwt from "jsonwebtoken";
 
 export const create = (req, res) => {
-    User.create(req.body)
-    .then(user => res.send(user))
-    .catch(error => res.send(error));
+  User.create(req.body)
+    .then(user => {
+      const token = jwt.sign({ id: user._id }, "secret", {
+        expiresIn: 86400
+      });
+      res.status(200).send(token);
+    })
+    .catch(error => res.status(400).send(error));
 };
