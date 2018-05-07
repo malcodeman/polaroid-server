96450bd5cd965aae009dd8a335cd75df029260e4
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-05-07 11:14:24 +0200

    Decode jwt

--- controllers/posts_controller.mjs
+++ controllers/posts_controller.mjs
@@ -1,4 +1,5 @@
 import Post from "../models/posts_model.mjs";
+import jwt from "jsonwebtoken";
 
 export const create = (req, res) => {
   Post.create(req.body)
@@ -7,7 +8,12 @@ export const create = (req, res) => {
 };
 
 export const get = (req, res) => {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, "secret");
+  console.log(decoded);
   Post.find({})
-    .then(posts => res.send(posts))
+    .then(posts => {
+      res.send(posts);
+    })
     .catch(error => console.log(error));
 };
