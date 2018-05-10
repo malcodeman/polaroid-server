50671a5f8d75e50202c4dc513dccd171c41b72f7
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-05-10 10:42:18 +0200

    User and post model validation

--- posts/posts_model.mjs
+++ posts/posts_model.mjs
@@ -2,8 +2,17 @@ import sequelize from "../connection.mjs";
 import Sequelize from "sequelize";
 
 const Post = sequelize.define("post", {
-  text: Sequelize.TEXT,
-  userId: Sequelize.INTEGER
+  text: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
 });
 
 export default Post;
