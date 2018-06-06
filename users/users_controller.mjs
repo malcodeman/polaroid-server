99a79034be7c6f1ea6b775dcafec1fc029e6f406
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-06-06 09:46:16 +0200

    Improves users endpoints

--- users/users_controller.mjs
+++ users/users_controller.mjs
@@ -1,9 +1,36 @@
 import User from "../users/users_model.mjs";
 
-export async function get(req, res, next) {
+export async function findById(req, res, next) {
   try {
-    const user = await User.findById(req.userId);
-    res.status(200).send(user);
+    const { id } = req.params;
+    const user = await User.findAll({
+      where: { id }
+    });
+    res.status(200).send(user[0]);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+export async function findAll(req, res, next) {
+  try {
+    const users = await User.findAll();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+export async function findMe(req, res, next) {
+  try {
+    const id = req.userId;
+    const me = await User.findAll({
+      where: { id },
+      attributes: {
+        exclude: ["password"]
+      }
+    });
+    res.status(200).send(me[0]);
   } catch (error) {
     res.status(400).send(error);
   }
--- users/users_routes.mjs
+++ users/users_routes.mjs
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { get } from "./users_controller.mjs";
+import { findById, findAll, findMe } from "./users_controller.mjs";
 import { requireAuthentication } from "../auth/auth_middleware.mjs";
 
 const router = express.Router();
 
 router.use(requireAuthentication);
-router.get("/me", get);
+
+router.get("/me", findMe);
+router.get("/:id", findById);
+router.get("/", findAll);
 
 export default router;
