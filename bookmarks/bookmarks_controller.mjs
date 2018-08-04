914454a3fc939330683176aa63f337637b9b281b
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-04 08:23:16 +0200

    Adds post when creating a bookmark

--- bookmarks/bookmarks_controller.mjs
+++ bookmarks/bookmarks_controller.mjs
@@ -1,9 +1,15 @@
 import Bookmark from "./bookmarks_model.mjs";
+import Post from "../posts/posts_model.mjs";
 
 export async function create(req, res, next) {
   try {
     const { postId } = req.body;
     const bookmark = await Bookmark.create({ userId: req.userId, postId });
+    const id = postId;
+    const post = await Post.findOne({
+      where: { id }
+    });
+    bookmark.dataValues.post = post.dataValues;
     res.status(200).send(bookmark);
   } catch (error) {
     res.status(400).send(error);
