5da1a5700b163d10b319753b7465a8ea15aebfe1
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-16 01:00:10 +0200

    Removes env var logs

--- src/connection.js
+++ src/connection.js
@@ -2,13 +2,6 @@ import Sequelize from "sequelize";
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-console.log(process.env.DB_NAME);
-
-console.log(DB_NAME);
-console.log(DB_USER);
-console.log(DB_PASSWORD);
-console.log(DB_HOST);
-
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
