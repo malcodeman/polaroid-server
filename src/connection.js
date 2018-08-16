4469e02faa6c73db77a66e0b5bd9ba2ba7a05d38
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-16 09:30:18 +0200

    Changes db connection config

--- src/connection.js
+++ src/connection.js
@@ -1,8 +1,8 @@
 import Sequelize from "sequelize";
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize(DATABASE_URL, {
   host: DB_HOST,
   dialect: "postgres",
   pool: {
