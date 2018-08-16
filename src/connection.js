42d2f65a29dfa675ea4f714f5a8e9529f05b343c
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-16 21:23:22 +0200

    Adds db dialect

--- src/connection.js
+++ src/connection.js
@@ -3,6 +3,7 @@ import Sequelize from "sequelize";
 const { DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
