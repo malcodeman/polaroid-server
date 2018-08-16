46e3b6840fe7e29f26540f9bc756562bb23762d7
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-08-16 21:16:55 +0200

    Updates database config

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,8 @@
 import Sequelize from "sequelize";
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  host: DB_HOST,
-  dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
